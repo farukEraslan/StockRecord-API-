f97fffa5666658f28073fa56c42e7b348814a9f8
farukEraslan <frkersln@gmail.com>
2023-01-15 15:34:13 +0300

    Kategori DataTable eklendi. API üstünden kategori bilgileri datatable'a basıldı.

---
+++ StokKontrol_API.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using StokKontrol_API.Entities.Entities;
+
+namespace StokKontrol_API.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        string uri = "https://localhost:7150";
+        public async Task<IActionResult> Index()
+        {
+            List<Category> kategoriler = new List<Category>();
+            using (var httpClient = new HttpClient()) 
+            {
+                using (var cevap = await httpClient.GetAsync($"{uri}/api/Category/TumKategorileriGetir"))
+                {
+                    string apiCevap = await cevap.Content.ReadAsStringAsync();
+                    kategoriler = JsonConvert.DeserializeObject<List<Category>>(apiCevap);
+                }
+            }
+            return View(kategoriler);
+        }
+    }
+}
